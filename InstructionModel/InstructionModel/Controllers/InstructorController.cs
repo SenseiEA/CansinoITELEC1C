dd85fe1b8ae3ce5fd3d78f2968aca207cd80366f
SenseiEA <134182302+SenseiEA@users.noreply.github.com>
2023-09-18 11:23:53 +0800

    Lab Activity 1
    Lab Activity 1 with Instruction Models

---
+++ InstructionModel/InstructionModel/Controllers/InstructorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InstructionModel.Models;
+
+namespace InstructionModel.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>
+        {
+            new Instructor()
+            {
+                Id = 1,
+                FirstName = "Noel",
+                LastName = "Cansino",
+                Rank = Rank.Professor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse("28/09/2020")
+            },
+            new Instructor()
+            {
+                Id = 2,
+                FirstName = "Ayaka",
+                LastName = "Kamisato",
+                Rank = Rank.AssistantProfessor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse("17/07/2021")
+            },
+            new Instructor()
+            {
+                Id = 3,
+                FirstName = "Zhong",
+                LastName = "Li",
+                Rank = Rank.AssociateProfessor,
+                IsTenured = IsTenured.Permanent,
+                HiringDate = DateOnly.Parse("01/12/2020")
+            },
+        };
+        public IActionResult Index()
+        {
+            return View(InstructorList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            Instructor? student = InstructorList.FirstOrDefault(st => st.Id == id);
+            if (student != null)
+            {
+                return View(student);
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ InstructionModel/InstructionModel/Models/Instructor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstructionModel.Models
+{
+    public enum Rank
+    {
+        Instructor, AssistantProfessor, AssociateProfessor, Professor
+    }
+
+    public enum IsTenured
+    {
+        Permanent, Probationary
+    }
+    public class Instructor
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public IsTenured IsTenured { get; set; }
+
+        public Rank Rank { get; set; }
+
+        public DateOnly HiringDate { get; set; }
+
+        public string Email { get; set; }
+       
+    }
+}
